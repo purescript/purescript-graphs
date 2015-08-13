ba0c4a9224b643c0701959646339d8bb7a2ab677
Gary Burgess <gary.burgess@gmail.com>
2015-08-13 15:24:48 +0100

    Updated dependencies, remove old Int import

--- src/Data/Graph.purs
+++ src/Data/Graph.purs
@@ -4,19 +4,18 @@ module Data.Graph (
   Edge(..),
   Graph(..),
   SCC(..),
-  
+
   vertices,
 
   scc,
   scc',
-  
+
   topSort,
   topSort'
   ) where
 
 import Prelude
 
-import Data.Int
 import Data.Maybe
 import Data.List
 import Data.Foldable
@@ -37,7 +36,7 @@ data Edge k = Edge k k
 -- | Edges refer to vertices using keys of type `k`.
 data Graph k v = Graph (List v) (List (Edge k))
 
-type Index = Int 
+type Index = Int
 
 -- | A strongly-connected component of a graph.
 -- |
@@ -47,7 +46,7 @@ type Index = Int
 data SCC v = AcyclicSCC v | CyclicSCC (List v)
 
 instance showSCC :: (Show v) => Show (SCC v) where
-  show (AcyclicSCC v) = "AcyclicSCC (" ++ show v ++ ")" 
+  show (AcyclicSCC v) = "AcyclicSCC (" ++ show v ++ ")"
   show (CyclicSCC vs) = "CyclicSCC " ++ show vs
 
 instance eqSCC :: (Eq v) => Eq (SCC v) where
@@ -65,26 +64,26 @@ scc :: forall v. (Eq v, Ord v) => Graph v v -> List (SCC v)
 scc = scc' id id
 
 -- | Compute the strongly connected components of a graph.
--- | 
+-- |
 -- | This function is a slight generalization of `scc` which allows key and value types
 -- | to differ.
 scc' :: forall k v. (Eq k, Ord k) => (v -> k) -> (k -> v) -> Graph k v -> List (SCC v)
 scc' makeKey makeVert (Graph vs es) = runPure (runST (do
-  index      <- newSTRef zero 
+  index      <- newSTRef zero
   path       <- newSTRef Nil
   indexMap   <- newSTRef M.empty
   lowlinkMap <- newSTRef M.empty
   components <- newSTRef Nil
 
-  (let 
+  (let
     indexOf v = indexOfKey (makeKey v)
-      
+
     indexOfKey k = do
       m <- readSTRef indexMap
       return $ M.lookup k m
-    
+
     lowlinkOf v = lowlinkOfKey (makeKey v)
-      
+
     lowlinkOfKey k = do
       m <- readSTRef lowlinkMap
       return $ M.lookup k m
@@ -97,7 +96,7 @@ scc' makeKey makeVert (Graph vs es) = runPure (runST (do
 
     strongConnect k = do
       let v = makeVert k
-      
+
       i <- readSTRef index
 
       modifySTRef indexMap   $ M.insert k i
@@ -123,7 +122,7 @@ scc' makeKey makeVert (Graph vs es) = runPure (runST (do
                    modifySTRef lowlinkMap $ M.alter (maybeMin index) k
 
       vIndex <- indexOfKey k
-      vLowlink <- lowlinkOfKey k        
+      vLowlink <- lowlinkOfKey k
 
       when (vIndex == vLowlink) $ do
         currentPath <- readSTRef path
@@ -131,15 +130,15 @@ scc' makeKey makeVert (Graph vs es) = runPure (runST (do
         modifySTRef components $ flip (++) (singleton (makeComponent newPath.component))
         writeSTRef path newPath.path
         return unit
-        
+
     makeComponent (Cons v Nil) | not (isCycle (makeKey v)) = AcyclicSCC v
     makeComponent vs = CyclicSCC vs
-    
+
     isCycle k = any (\(Edge k1 k2) -> k1 == k && k2 == k) es
    in go vs)))
 
 popUntil :: forall k v. (Eq k) => (v -> k) -> v -> List v -> List v -> { path :: List v, component :: List v }
-popUntil _       _ Nil           popped = { path: Nil, component: popped } 
+popUntil _       _ Nil           popped = { path: Nil, component: popped }
 popUntil makeKey v (Cons w path) popped | makeKey v == makeKey w = { path: path, component: Cons w popped }
 popUntil makeKey v (Cons w ws)   popped = popUntil makeKey v ws (Cons w popped)
 
@@ -154,7 +153,7 @@ topSort :: forall v. (Eq v, Ord v) => Graph v v -> List v
 topSort = topSort' id id
 
 -- | Topologically sort the vertices of a graph
--- | 
+-- |
 -- | This function is a slight generalization of `scc` which allows key and value types
 -- | to differ.
 topSort' :: forall k v. (Eq k, Ord k) => (v -> k) -> (k -> v) -> Graph k v -> List v
