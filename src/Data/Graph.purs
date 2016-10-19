ce9588fa7d9f555fddf9df825c2b4b9681d86325
Phil Freeman <paf31@cantab.net>
2016-10-18 20:39:44 -0700

    More rewriting

--- src/Data/Graph.purs
+++ src/Data/Graph.purs
@@ -1,17 +1,23 @@
 -- | A data structure and functions for graphs
 
 module Data.Graph
-  ( Graph(..)
-  , topSort
+  ( Graph
+  , unfoldGraph
+  , vertices
+  , lookup
+  , outEdges
+  , topologicalSort
   ) where
 
 import Prelude
-
-import Data.Foldable (foldl)
+import Data.Foldable (class Foldable, foldl)
 import Data.List (List(..))
-import Data.Map (Map, lookup, delete, findMin)
+import Data.List as L
+import Data.Map (Map)
+import Data.Map as M
 import Data.Maybe (Maybe(..), maybe)
 import Data.Monoid (mempty)
+import Data.Tuple (Tuple(..))
 
 -- | A graph with vertices of type `v`.
 -- |
@@ -20,32 +26,70 @@ newtype Graph k v = Graph
   { vertices :: Map k { value :: v, out :: List k }
   }
 
-type SortState k =
-  { unvisited :: Map k Unit
+-- | Unfold a graph from a collection of keys and functions which label keys
+-- | and specify out-edges.
+unfoldGraph
+  :: forall f k v out
+   . (Ord k, Functor f, Foldable f, Foldable out)
+  => f k
+  -> (k -> v)
+  -> (k -> out k)
+  -> Graph k v
+unfoldGraph ks label edges =
+  Graph { vertices: M.fromFoldable (map (\k ->
+            Tuple k { value: label k
+                    , out: L.fromFoldable (edges k)
+                    }) ks)
+        }
+
+-- | List all vertices in a graph.
+vertices :: forall k v. Graph k v -> List v
+vertices (Graph g) = map _.value (M.values g.vertices)
+
+-- | Lookup a vertex by its key.
+lookup :: forall k v. Ord k => k -> Graph k v -> Maybe v
+lookup k (Graph g) = map _.value (M.lookup k g.vertices)
+
+-- | Get the keys which are directly accessible from the given key.
+outEdges :: forall k v. Ord k => k -> Graph k v -> Maybe (List k)
+outEdges k (Graph g) = map _.out (M.lookup k g.vertices)
+
+type SortState k v =
+  { unvisited :: Map k { value :: v, out :: List k }
   , result :: List k
   }
 
--- | Topologically sort the vertices of a graph
-topSort :: forall k v. Ord k => Graph k v -> List k
-topSort (Graph { vertices }) =
+-- | Topologically sort the vertices of a graph.
+-- |
+-- | If the graph contains cycles, then the behavior is undefined.
+topologicalSort :: forall k v. Ord k => Graph k v -> List k
+topologicalSort (Graph g) =
     go initialState
   where
-    go :: SortState k -> List k
+    go :: SortState k v -> List k
     go state@{ unvisited, result } =
-      case findMin unvisited of
+      case M.findMin unvisited of
         Just { key } -> go (visit state key)
         Nothing -> result
 
-    visit :: SortState k -> k -> SortState k
-    visit state k =
-      let finish :: SortState k -> SortState k
-          finish { unvisited, result } =
-            { result: Cons k result
-            , unvisited: delete k unvisited
-            }
-      in finish (foldl visit state (maybe mempty _.out (lookup k vertices)))
+    visit :: SortState k v -> k -> SortState k v
+    visit state k
+      | k `M.member` state.unvisited =
+        let finish :: SortState k v -> SortState k v
+            finish { unvisited, result } =
+              { result: Cons k result
+              , unvisited: unvisited
+              }
 
-    initialState :: SortState k
-    initialState = { unvisited: void vertices
+            start :: SortState k v
+            start =
+              { result: state.result
+              , unvisited: M.delete k state.unvisited
+              }
+        in finish (foldl visit start (maybe mempty _.out (M.lookup k g.vertices)))
+      | otherwise = state
+
+    initialState :: SortState k v
+    initialState = { unvisited: g.vertices
                    , result: Nil
                    }
--- test/Main.purs
+++ test/Main.purs
@@ -1,18 +1,16 @@
 module Test.Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Data.Graph (Graph(..), topSort)
-import Data.List (fromFoldable, range)
-import Data.Monoid (mempty)
-import Data.Map as M
 import Data.Foldable (traverse_)
-import Data.Tuple (Tuple(..))
+import Data.Graph (unfoldGraph, topologicalSort)
+import Data.List (range)
 
 main :: Eff (console :: CONSOLE) Unit
 main = do
-  let sorted = topSort graph
-      graph = Graph { vertices: M.fromList (map double (range 1 1000)) }
-      double x = Tuple x { value: x, out: fromFoldable (if x <= 500 then [x * 2] else []) }
-  traverse_ logShow sorted
+  let double x | x * 3 < 1000 = [x * 2, x * 3]
+               | otherwise    = []
+      graph = unfoldGraph (range 1 1000) id double
+  traverse_ logShow (topologicalSort graph)
