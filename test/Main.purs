c3d03e6e5674f03869b2c83b6ee3af00222a954c
Gary Burgess <gary.burgess@gmail.com>
2018-06-06 21:01:44 +0100

    Updates for 0.12

--- test/Main.purs
+++ test/Main.purs
@@ -1,14 +1,15 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (foreachE, Eff)
-import Control.Monad.Eff.Console (CONSOLE, logShow)
+
+import Effect (Effect, foreachE)
+import Effect.Console (logShow)
 import Data.Graph (unfoldGraph, topologicalSort)
 import Data.List (toUnfoldable, range)
 
-main :: Eff (console :: CONSOLE) Unit
+main :: Effect Unit
 main = do
   let double x | x * 2 < 100000 = [x * 2]
                | otherwise      = []
-      graph = unfoldGraph (range 1 100000) id double
+      graph = unfoldGraph (range 1 100000) identity double
   foreachE (toUnfoldable (topologicalSort graph)) logShow
